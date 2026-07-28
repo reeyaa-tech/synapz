a12b92a52b2bdfa08ca6805243c9f0f15cbc3840
reeyaa-tech <reeyaa0812@gmail.com>
2026-07-29 02:09:29 +0530

    Fix socket export syntax in socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(''https://synapz-p7bs.onrender.com', {
+export const socket = io('https://synapz-p7bs.onrender.com', {
   autoConnect: true,
   transports: ["websocket"]
 });
